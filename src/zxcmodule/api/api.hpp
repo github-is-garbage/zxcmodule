83d10ac2ec3b4806b895437eb1d4c5ff3ea1158a
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-11-21 17:44:41 -0500

    Add PushCFunction to API base

--- src/zxcmodule/api/api.hpp
+++ src/zxcmodule/api/api.hpp
@@ -13,6 +13,13 @@ public:
 		this->pLuaInterface = pLua;
 	}
 
+	void PushCFunction(GarrysMod::Lua::CFunc pFunction, const char* pszName, int iIndex = -3)
+	{
+		this->pLuaInterface->PushString(pszName);
+		this->pLuaInterface->PushCFunction(pFunction);
+		this->pLuaInterface->RawSet(iIndex);
+	}
+
 	virtual void Init() = 0;
 	virtual void UnInit() = 0;
 };
