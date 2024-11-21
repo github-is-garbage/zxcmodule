7a5d6cfdb6c1e94c59887c122899bf8550a0d151
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-11-21 16:26:23 -0500

    Test gmod interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,7 @@
-#include <Windows.h>
-#include <iostream>
+#include <GarrysMod/Lua/Interface.h>
 
-BOOL WINAPI DllMain(
-    HINSTANCE hinstDLL,  // handle to DLL module
-    DWORD fdwReason,     // reason for calling function
-    LPVOID lpvReserved )  // reserved
+GMOD_MODULE_OPEN()
 {
-    // Perform actions based on the reason for calling.
-    switch( fdwReason )
-    {
-        case DLL_PROCESS_ATTACH:
-			AllocConsole();
-            break;
-
-        case DLL_THREAD_ATTACH:
-         // Do thread-specific initialization.
-            break;
-
-        case DLL_THREAD_DETACH:
-         // Do thread-specific cleanup.
-            break;
-
-        case DLL_PROCESS_DETACH:
-
-            if (lpvReserved != nullptr)
-            {
-                break; // do not do cleanup if process termination scenario
-            }
-
-         // Perform any necessary cleanup.
-            break;
-    }
-    return TRUE;  // Successful DLL_PROCESS_ATTACH.
+	LUA->ThrowError("hey");
+	return 0;
 }
