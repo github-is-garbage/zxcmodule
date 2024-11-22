79a25a105ea34132d63e32f131411e9203546e02
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-11-21 22:40:40 -0500

    Remove pLuaInterface from API base
    Some stupid compile error oh well

--- src/zxcmodule/api/api.hpp
+++ src/zxcmodule/api/api.hpp
@@ -5,19 +5,11 @@
 class API
 {
 public:
-	GarrysMod::Lua::ILuaInterface* pLuaInterface;
-
-public:
-	API(GarrysMod::Lua::ILuaInterface* pLua)
+	void PushCFunction(GarrysMod::Lua::ILuaInterface* pLuaInterface, GarrysMod::Lua::CFunc pFunction, const char* pszName, int iIndex = -3)
 	{
-		this->pLuaInterface = pLua;
-	}
-
-	void PushCFunction(GarrysMod::Lua::CFunc pFunction, const char* pszName, int iIndex = -3)
-	{
-		this->pLuaInterface->PushString(pszName);
-		this->pLuaInterface->PushCFunction(pFunction);
-		this->pLuaInterface->RawSet(iIndex);
+		pLuaInterface->PushString(pszName);
+		pLuaInterface->PushCFunction(pFunction);
+		pLuaInterface->RawSet(iIndex);
 	}
 
 	virtual void Init() = 0;
