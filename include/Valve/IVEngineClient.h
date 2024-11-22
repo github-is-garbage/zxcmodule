d1145538f823944d2320251d2ea7d7a64a2dd0fd
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-11-21 23:27:17 -0500

    Add engine.ShouldDrawBox

--- include/Valve/IVEngineClient.h
+++ include/Valve/IVEngineClient.h
@@ -1,11 +1,14 @@
 #pragma once
 
-#include "VMatrix.h"
+#include <Valve/Vector.h>
+#include <Valve/VMatrix.h>
 #include <vmt/vmt.hpp>
 
 class IVEngineClient
 {
 public:
 	VPROXY(GetScreenSize, 5, void, (int& x, int& y), x, y)
+	VPROXY(IsBoxInViewCluster, 32, int, (const Vector& Mins, const Vector& Maxs), Mins, Maxs)
+	VPROXY(CullBox, 33, bool, (const Vector& Mins, const Vector& Maxs), Mins, Maxs)
 	VPROXY(WorldToScreenMatrix, 36, VMatrix&, (void))
 };
--- src/zxcmodule/api/engine.hpp
+++ src/zxcmodule/api/engine.hpp
@@ -4,6 +4,30 @@
 #include <GarrysMod/Lua/Interface.h>
 #include "../globals.h"
 
+LUA_FUNCTION(ShouldDrawBox)
+{
+	LUA->CheckType(1, GarrysMod::Lua::Type::Vector);
+	LUA->CheckType(2, GarrysMod::Lua::Type::Vector);
+
+	Vector Mins = LUA->GetVector(1);
+	Vector Maxs = LUA->GetVector(2);
+
+	if (!pGlobals->pPointers->pEngineClient->IsBoxInViewCluster(Mins, Maxs))
+	{
+		LUA->PushBool(false);
+		return 1;
+	}
+
+	if (pGlobals->pPointers->pEngineClient->CullBox(Mins, Maxs))
+	{
+		LUA->PushBool(false);
+		return 1;
+	}
+
+	LUA->PushBool(true);
+	return 1;
+}
+
 LUA_FUNCTION(GetWorldToScreenMatrix)
 {
 	VMatrix vmWorldToScreen = pGlobals->pPointers->pEngineClient->WorldToScreenMatrix();
@@ -21,6 +45,7 @@ public:
 		pGlobals->pLuaInterface->PushString("engine");
 		pGlobals->pLuaInterface->CreateTable();
 		{
+			this->PushCFunction(pGlobals->pLuaInterface, ShouldDrawBox, "ShouldDrawBox");
 			this->PushCFunction(pGlobals->pLuaInterface, GetWorldToScreenMatrix, "GetWorldToScreenMatrix");
 		}
 		pGlobals->pLuaInterface->RawSet(-3);
