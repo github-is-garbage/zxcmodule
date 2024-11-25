6e90c3e1e7da2bbe92e668e54086a08c434842d0
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-11-25 15:44:40 -0500

    Clean up pClientState grab
    And also comment why it's like that

--- src/zxcmodule/pointers.cpp
+++ src/zxcmodule/pointers.cpp
@@ -6,7 +6,13 @@
 void Pointers::Init()
 {
 	this->pEngineClient = this->GetInterface<IVEngineClient*>("engine.dll", VENGINE_CLIENT_INTERFACE_VERSION);
-	this->pClientState = Memory::RelativeToReal<CClientState*>(VMT::GetMethodPointerAt((char***)this->pEngineClient, VENGINE_CLIENT_ISPAUSED_OFFSET), 3, 7);
+
+	/*
+		This nabs the CClientState instantiated within the engine by going down the instructions to find the push
+		https://github.com/lua9520/source-engine-2018-hl2_src/blob/master/engine/cdll_engine_int.cpp#L1310
+	*/
+	char* pEngineIsPaused = VMT::GetMethodPointerAt((char***)this->pEngineClient, VENGINE_CLIENT_ISPAUSED_OFFSET);
+	this->pClientState = Memory::RelativeToReal<CClientState*>(pEngineIsPaused, 3, 7);
 }
 
 void Pointers::UnInit()
