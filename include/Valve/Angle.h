b2715ce29f136faf875829224ea675643427e620
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-11-26 01:52:31 -0500

    Add constructors to Angle and Vector

--- include/Valve/Angle.h
+++ include/Valve/Angle.h
@@ -5,6 +5,15 @@ class QAngle
 public:
 	float x, y, z;
 
+	QAngle() { x = y = z = 0.0f; }
+
+	QAngle(float x, float y, float z)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+
 	float operator[](int i) const;
 	float& operator[](int i);
 };
--- include/Valve/Vector.h
+++ include/Valve/Vector.h
@@ -5,6 +5,15 @@ class Vector
 public:
 	float x, y, z;
 
+	Vector() { x = y = z = 0.0f; }
+
+	Vector(float x, float y, float z)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+
 	float operator[](int i) const;
 	float& operator[](int i);
 };
