11bdb759c9ce82f9a84b02d608830b0aede4c589
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-11-26 01:50:28 -0500

    Add [] index to Angle and Vector

--- include/Valve/Angle.h
+++ include/Valve/Angle.h
@@ -4,4 +4,7 @@ class QAngle
 {
 public:
 	float x, y, z;
+
+	float operator[](int i) const;
+	float& operator[](int i);
 };
--- include/Valve/Vector.h
+++ include/Valve/Vector.h
@@ -4,4 +4,7 @@ class Vector
 {
 public:
 	float x, y, z;
+
+	float operator[](int i) const;
+	float& operator[](int i);
 };
