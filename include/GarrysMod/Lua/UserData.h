35948c89e543c5b99f9a607e403eeef38c8e5a14
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-11-26 22:13:04 -0500

    Convert indentation to tabs

--- include/GarrysMod/Lua/UserData.h
+++ include/GarrysMod/Lua/UserData.h
@@ -2,17 +2,17 @@
 #define GARRYSMOD_LUA_USERDATA_H
 
 #ifdef GMOD_ALLOW_DEPRECATED
-    namespace GarrysMod
-    {
-        namespace Lua
-        {
-            struct UserData
-            {
-                void*         data;
-                unsigned char type;
-            };
-        }
-    }
+	namespace GarrysMod
+	{
+		namespace Lua
+		{
+			struct UserData
+			{
+				void*         data;
+				unsigned char type;
+			};
+		}
+	}
 #endif
 
 #endif
